f7d1009034057b55cdabca2467bc985285655c30
ServicedeskPoint2IT <167416859+ServicedeskPoint2IT@users.noreply.github.com>
2024-04-22 11:20:15 +0200

    Update debloat-intune-script.ps1

--- De-Bloat/debloat-intune-script.ps1
+++ De-Bloat/debloat-intune-script.ps1
@@ -12,7 +12,7 @@ Else {
 $templateFilePath = "C:\ProgramData\Debloat\removebloat.ps1"
 
 Invoke-WebRequest `
--Uri "https://raw.githubusercontent.com/ServicedeskPoint2IT/WinDeBloat/main/De-Bloat/debloat-intune-script.ps1" `
+-Uri "https://raw.githubusercontent.com/ServicedeskPoint2IT/WinDeBloat/main/De-Bloat/RemoveBloat.ps1" `
 -OutFile $templateFilePath `
 -UseBasicParsing `
 -Headers @{"Cache-Control"="no-cache"}
